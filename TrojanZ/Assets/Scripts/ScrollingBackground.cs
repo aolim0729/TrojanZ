bdf0c289d2664a6418b70a378f4248d3a7ebe38f
aolim0729 <aolim@iskolarngbayan.pup.edu.ph>
2021-02-17 23:06:10 +0800

    1st Update
    Added
    - Background
    - Player

---
+++ TrojanZ/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    
+    public float offset;
+    
+    public Vector2 startPosition;
+    
+    public float newXposition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newXposition = Mathf.Repeat(Time.time * -moveSpeed, offset);
+
+        transform.position = startPosition + Vector2.right * newXposition;
+    }
+}

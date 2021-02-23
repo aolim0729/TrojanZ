27c3ce49d7d9282f3aac4146df09a48667b2d1f2
aolim0729 <aolim@iskolarngbayan.pup.edu.ph>
2021-02-23 19:05:36 +0800

    Added Main Menu
    Added Main Menu scene in game

---
+++ TrojanZ/Assets/Scripts/Title.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Title : MonoBehaviour
+{
+    private const string TITLE_PC = "Press X";
+    private const string TITLE_MOBILE = "Tap To Start";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
